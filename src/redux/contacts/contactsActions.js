73448499ff547ad2b2675799a2164df8bac83d5b
anastasiia-matushenko <77169504+anastasiia-matushenko@users.noreply.github.com>
2022-11-01 23:22:51 +0300

    fix

--- src/redux/contacts/contactsActions.js
+++ src/redux/contacts/contactsActions.js
@@ -1,6 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 
 export const addContact = createAction('contacts/add');
-export const deleteContact = createAction('contacts/add');
+export const deleteContact = createAction('contacts/delete');
 
 export const filterContacts = createAction('contacts/filter');
